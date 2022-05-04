567a262d0e192f77bb20ef58aa3f6c696625e6a3
Siva Pallem <74588031+Siva-PNV@users.noreply.github.com>
2022-05-04 09:56:21 +0530

    Change POST to GET for login
    Change POST to GET for login

--- src/main/java/com/tweetapp/tweetapp/controller/AuthController.java
+++ src/main/java/com/tweetapp/tweetapp/controller/AuthController.java
@@ -8,9 +8,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins="http://localhost:4200")
@@ -37,8 +39,11 @@ public class AuthController {
 
     }
 
-    @PostMapping("login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginCredentials loginCredentials) {
+    @GetMapping("login")
+    public ResponseEntity<String> loginUser(@RequestParam String userName,@RequestParam String password) {
+        LoginCredentials loginCredentials=new LoginCredentials();
+        loginCredentials.setLoginId(userName);
+        loginCredentials.setPassword(password);
             if(usersService.checkUser(loginCredentials)){
                 return new ResponseEntity<>("success",HttpStatus.OK);
             }
